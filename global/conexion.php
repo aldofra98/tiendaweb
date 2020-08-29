61799f5f01bf42a2ce96242be525424e8c3199fc
aldofra98 <62958912+aldofra98@users.noreply.github.com>
2020-08-29 17:00:00 -0500

    Add files via upload

---
+++ global/conexion.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+function getPDO () {
+    try {
+//print "¡conetado perooooo: <br/>";
+        $pdo = new PDO('mysql:host=localhost;dbname=tienda', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = $pdo->prepare('SELECT * FROM tblproductos');
+       $sql->execute();
+       $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+        return $resultado;
+      }  
+      catch(PDOException $e){
+      //  print "¡Error!: <br/>";  
+        return null;
+      }
+  }
+?>
